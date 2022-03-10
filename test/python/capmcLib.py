af1e44e5f2e876a9ac18b30504de55a61d0af0b7
Andrew Nieuwsma <76961724+anieuwsma-hpe@users.noreply.github.com>
2022-03-10 05:11:37 -0600

    go mod vendor, update changelog, remove old test resources

--- test/python/capmcLib.py
+++ test/python/capmcLib.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #  MIT License
 #
-#  (C) Copyright [2019-2021] Hewlett Packard Enterprise Development LP
+#  (C) Copyright [2019-2022] Hewlett Packard Enterprise Development LP
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a
 #  copy of this software and associated documentation files (the "Software"),
@@ -46,134 +46,6 @@ from time import sleep
     #json=$(eval $CMD)
     #echo $json
 #}
-
-"""
-    getNodeState(nid)
-
-    Queries the state manager to get the current status of a single node
-
-    Arguments:
-        string nid
-
-    Returns:
-        A dictionary containing an error code, an error string, and a state
-        string.
-        Dictionary:
-            int errcode   - Error returned from Popen's communicate() or -1 if
-                            there was no status returned. Default: 0
-            string errstr - Error indicated by the failure type. Default: ""
-            string state  - State of the node: on, off, halt, etc. Default: ""
-"""
-def getNodeState(nid):
-    #command = "curl -k -X GET https://slice-sms.us.cray.com/apis/smd/hsm/v1/State/Components/ByNID/"+nid
-    command = "cray hsm State Components ByNID describe "+nid
-    proc = Popen(split(command), stdout=PIPE, stderr=PIPE)
-    proc.wait()
-    stdout, stderr = proc.communicate()
-
-    state = dict()
-    state["errcode"] = 0
-    state["errstr"] = ""
-    state["state"] = ""
-
-    if proc.returncode != 0:
-        state["errcode"] = proc.returncode
-        state["errstr"] = stderr.decode("utf-8").split(":")[2].lstrip()
-        return state
-
-    if stdout == None:
-        state["errcode"] = -1
-        state["errstr"] = "No status returned"
-        return state
-
-    stateMap = loads(stdout)
-    state["state"] = stateMap["State"]
-
-    return state
-
-"""
-    nodePower(opt, nid)
-
-    Uses craycli to send a power off request to capmc for the specific node
-
-    Arguments:
-        string opt  - "on", "off", "reinit"
-        string nid
-
-    Returns:
-        A dictinoary containing an error code and an error string.
-
-        Dictionary:
-            int errcode   - Error returned from Popen's communicate() or -1 if
-                            there was no response. Default: 0
-            string errstr - Error indicated by the failure type. Default: ""
-"""
-def nodePower(opt, nid):
-    command = "cray capmc node_"+opt+" create --nids "+nid
-    proc = Popen(split(command), stdout=PIPE, stderr=PIPE)
-    proc.wait()
-    stdout, stderr = proc.communicate()
-
-    ret = dict()
-    ret["errcode"] = 0
-    ret["errstr"] = ""
-
-    if proc.returncode != 0:
-        ret["errcode"] = proc.returncode
-        ret["errstr"] = stderr.decode("utf-8").split(":")[2].lstrip()
-        return ret
-
-    if stdout == None:
-        ret["errcode"] = -1
-        ret["errstr"] = "Nothing returned"
-        return ret
-
-    return ret
-
-"""
-    nodeWait(nid, targ, maxloops)
-
-    Checks to see if the status of the nid matches targ maxloops times.
-
-    Arguments:
-        string nid
-        string targ     - Standby, Ready, etc.
-        string maxloops
-
-    Returns:
-        A dictionary containing an error code and an error string.
-
-        Dictionary:
-            int errcode   - Error returned from Popen's communicate() or -1 if
-                            there was no response. Default: 0
-            string errstr - Error indicated by the failure type. Default: ""
-"""
-def nodeWait(nid, targ, maxloops):
-    curState = getNodeState(nid)
-    ret = dict()
-    ret["errcode"] = 0
-    ret["errstr"] = ""
-    if curState["errcode"] != 0:
-        ret["errcode"] = curState["errcode"]
-        ret["errstr"] = curState["errstr"]
-        return ret
-
-    loops = 1
-    while curState["state"] != targ:
-        sleep(1)
-
-        curState = getNodeState(nid)
-        if curState["errcode"] != 0:
-            ret["errcode"] = curState["errcode"]
-            ret["errstr"] = curState["errstr"]
-            return ret
-
-        if loops > maxloops:
-            ret["errcode"] = -1
-            ret["errstr"] = "Timeout exceeded"
-            return ret
-
-    return ret
 
 """
     getXnameState(xname)
