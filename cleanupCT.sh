bdf865ff92a42449b127a47d0525593c5e381f02
Andrew Nieuwsma <76961724+anieuwsma-hpe@users.noreply.github.com>
2022-07-14 09:56:36 -0500

    added a partial dev environment

---
+++ cleanupCT.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+#
+# MIT License
+#
+# (C) Copyright [2022] Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+set -x
+
+# Add .exe if running in a WSL environment
+if $(uname -r | grep -q "Microsoft"); then
+    shopt -s expand_aliases
+    alias docker-compose=docker-compose.exe
+fi
+
+# Configure docker compose
+export COMPOSE_PROJECT_NAME=local
+export COMPOSE_FILE=docker-compose.developer.full.yaml
+
+args="-f $COMPOSE_FILE -p $COMPOSE_PROJECT_NAME"
+
+echo "COMPOSE_PROJECT_NAME: ${COMPOSE_PROJECT_NAME}"
+echo "COMPOSE_FILE: $COMPOSE_FILE"
+
+
+function cleanup() {
+  docker-compose $args down
+  if ! [[ $? -eq 0 ]]; then
+    echo "Failed to decompose environment!"
+    exit 1
+  fi
+  exit $1
+}
+
+
+# Get the base containers running
+echo "Stopping containers..."
+cleanup 0
